343b19abe73b7055181ecf1cb874f6d63e35e336
小草 <2802083538@qq.com>
2021-04-16 14:02:31 +0800

    更新 main.dart

--- Dart/lib/main.dart
+++ Dart/lib/main.dart
@@ -1,4 +1,11 @@
 import 'api/pixiv_request.dart';
-void main(){
 
+void main() {
+  PixivRequest.instance.getRanking(1, true, decodeException: (e, response) {
+    print('反序列化异常:$e\n响应:$response');
+  }, requestException: (e) {
+    print('请求异常:$e\n响应:${e.response}');
+  }).then((ranking) {
+    print(ranking);
+  });
 }
